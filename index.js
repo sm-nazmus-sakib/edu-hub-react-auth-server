601e16504527e5626cec25bb61dbe31075e3cbcd
sm-nazmus-sakib <nazmussakibnwu@gmal.com>
2022-10-28 12:25:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedCourse = courses.find((course) => course.id === id);
+  res.send(selectedCourse);
+});
+
+app.listen(port, () => {
+  console.log(`The Server is Running On The Port ${port}`);
+});
+
+
+
+
